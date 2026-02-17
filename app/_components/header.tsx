df49160c77a491cf302f4fbc1e0751f1a3b90798
Maycon Cezar <maycon.cezar.souza7@gmail.com>
2026-02-17 12:06:49 -0300

    feat: add sidebar

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,7 +2,18 @@ import React from "react"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
 import { Button } from "./ui/button"
-import { MenuIcon } from "lucide-react"
+import { Calendar1Icon, HomeIcon, LogOut, MenuIcon } from "lucide-react"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet"
+import { quickSearchOptions } from "./_constants/searchQuick"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import Link from "next/link"
 
 const Header = () => {
   return (
@@ -14,9 +25,87 @@ const Header = () => {
           height={18}
           width={120}
         />
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="overflow-y-auto">
+            <SheetHeader className="">
+              <SheetTitle className="text-start text-lg font-bold">
+                Menu
+              </SheetTitle>
+            </SheetHeader>
+
+            <div className="flex items-center justify-start gap-3 border-b border-solid py-5">
+              <Avatar>
+                <AvatarImage
+                  alt=""
+                  src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D"
+                />
+              </Avatar>
+
+              <div className="flex flex-col">
+                <p className="truncate text-base font-bold">Maycon Cezar</p>
+                <p className="truncate text-xs font-normal">
+                  maycon.cezar.souza@gmail.com
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              <SheetClose >
+                <Button
+                  className="flex items-center justify-start text-sm font-normal w-full"
+                  variant="ghost"
+                >
+                  <Link href="/" className="flex items-center gap-2">
+                    <HomeIcon size={18} />
+                    Ìnicio
+                  </Link>
+                </Button>
+              </SheetClose>
+
+              <Button
+                className="flex items-center justify-start text-sm font-normal"
+                variant="ghost"
+              >
+                <Calendar1Icon size={18} />
+                Agendamentos
+              </Button>
+            </div>
+
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              {quickSearchOptions.map((option) => (
+                <Button
+                  className="flex items-center justify-start text-sm font-normal"
+                  variant="ghost"
+                  key={option.title}
+                >
+                  <Image
+                    alt={option.title}
+                    src={option.imageUrl}
+                    width={18}
+                    height={18}
+                  />
+                  {option.title}
+                </Button>
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-2 py-5">
+              <Button
+                className="flex items-center justify-start text-sm font-normal"
+                variant="ghost"
+              >
+                <LogOut />
+                Sair da conta
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   )
