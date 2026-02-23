364fbbbed991bab898f8b5b2d593a699760f0504
Maycon Cezar <maycon.cezar.souza7@gmail.com>
2026-02-23 11:20:07 -0300

    feat: add sidebar in Dialog connect account google

--- app/_components/sidebar-button.tsx
+++ app/_components/sidebar-button.tsx
@@ -1,28 +1,48 @@
-import React from 'react'
-import { Sheet, SheetTrigger,SheetContent,SheetHeader, SheetClose,SheetTitle } from './ui/sheet'
-import { Avatar,AvatarImage } from './ui/avatar'
-import { Button } from './ui/button'
-import { Calendar1Icon, HomeIcon, LogOut, MenuIcon } from 'lucide-react'
-import Link from 'next/link'
-import { quickSearchOptions } from './_constants/searchQuick'
-import Image from 'next/image' 
+import React from "react"
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetHeader,
+  SheetClose,
+  SheetTitle,
+} from "./ui/sheet"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import { Button } from "./ui/button"
+import {
+  Calendar1Icon,
+  HomeIcon,
+  LogInIcon,
+  LogOut,
+  MenuIcon,
+} from "lucide-react"
+import Link from "next/link"
+import { quickSearchOptions } from "./_constants/searchQuick"
+import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTrigger,
+} from "./ui/dialog"
+import { DialogTitle } from "@radix-ui/react-dialog"
+
+import logoGoogle from "../../public/google.svg";
 
 const SidebarButton = () => {
   return (
     <Sheet>
-          <SheetTrigger asChild>
-            <Button size="icon" variant="outline">
-              <MenuIcon />
-            </Button>
-          </SheetTrigger>
-          <SheetContent className="overflow-y-auto">
-            <SheetHeader className="">
-              <SheetTitle className="text-start text-lg font-bold">
-                Menu
-              </SheetTitle>
-            </SheetHeader>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="overflow-y-auto">
+        <SheetHeader className="">
+          <SheetTitle className="text-start text-lg font-bold">Menu</SheetTitle>
+        </SheetHeader>
 
-            <div className="flex items-center justify-start gap-3 border-b border-solid py-5">
+        {/*<div className="flex items-center justify-start gap-3 border-b border-solid py-5">
               <Avatar>
                 <AvatarImage
                   alt=""
@@ -36,59 +56,82 @@ const SidebarButton = () => {
                   maycon.cezar.souza@gmail.com
                 </p>
               </div>
-            </div>
+            </div>*/}
 
-            <div className="flex flex-col gap-2 border-b border-solid py-5">
-              <SheetClose >
-                <Button
-                  className="flex items-center justify-start text-sm font-normal w-full"
-                  variant="ghost"
-                >
-                  <Link href="/" className="flex items-center gap-2">
-                    <HomeIcon size={18} />
-                    Ìnicio
-                  </Link>
-                </Button>
-              </SheetClose>
+        <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+          <h2 className="text-base font-bold">Olá. Faça seu login!</h2>
 
-              <Button
-                className="flex items-center justify-start text-sm font-normal"
-                variant="ghost"
-              >
-                <Calendar1Icon size={18} />
-                Agendamentos
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size="sm" >
+                <LogInIcon size={18} />
               </Button>
-            </div>
+            </DialogTrigger>
+            <DialogContent className="flex w-[90%] flex-col items-center">
+              <DialogTitle className="text-base font-bold">
+                Faça login na plataforma
+              </DialogTitle>
+              <DialogDescription className="text-sm text-gray-600">
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+              <Button variant="outline" className="w-full">
+                <Image alt="login usando conta do google" src={logoGoogle} width={18} height={18}/>
+                Google</Button>
+            </DialogContent>
+          </Dialog>
+        </div>
 
-            <div className="flex flex-col gap-2 border-b border-solid py-5">
-              {quickSearchOptions.map((option) => (
-                <Button
-                  className="flex items-center justify-start text-sm font-normal"
-                  variant="ghost"
-                  key={option.title}
-                >
-                  <Image
-                    alt={option.title}
-                    src={option.imageUrl}
-                    width={18}
-                    height={18}
-                  />
-                  {option.title}
-                </Button>
-              ))}
-            </div>
+        <div className="flex flex-col gap-2 border-b border-solid py-5">
+          <SheetClose>
+            <Button
+              className="flex w-full items-center justify-start text-sm font-normal"
+              variant="ghost"
+            >
+              <Link href="/" className="flex items-center gap-2">
+                <HomeIcon size={18} />
+                Ìnicio
+              </Link>
+            </Button>
+          </SheetClose>
 
-            <div className="flex flex-col gap-2 py-5">
-              <Button
-                className="flex items-center justify-start text-sm font-normal"
-                variant="ghost"
-              >
-                <LogOut />
-                Sair da conta
-              </Button>
-            </div>
-          </SheetContent>
-        </Sheet>
+          <Button
+            className="flex items-center justify-start text-sm font-normal"
+            variant="ghost"
+          >
+            <Calendar1Icon size={18} />
+            Agendamentos
+          </Button>
+        </div>
+
+        <div className="flex flex-col gap-2 border-b border-solid py-5">
+          {quickSearchOptions.map((option) => (
+            <Button
+              className="flex items-center justify-start text-sm font-normal"
+              variant="ghost"
+              key={option.title}
+            >
+              <Image
+                alt={option.title}
+                src={option.imageUrl}
+                width={18}
+                height={18}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
+        <div className="flex flex-col gap-2 py-5">
+          <Button
+            className="flex items-center justify-start text-sm font-normal"
+            variant="ghost"
+          >
+            <LogOut />
+            Sair da conta
+          </Button>
+        </div>
+      </SheetContent>
+    </Sheet>
   )
 }
 
