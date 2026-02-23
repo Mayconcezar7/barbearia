ed341c9f2aa201c17aae74c141f69f059f550143
Maycon Cezar <maycon.cezar.souza7@gmail.com>
2026-02-23 13:48:49 -0300

    chore: add connect account with Google  add setup Better auth

--- app/_components/sidebar-button.tsx
+++ app/_components/sidebar-button.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+"use client"
+
 import {
   Sheet,
   SheetTrigger,
@@ -27,9 +28,36 @@ import {
 } from "./ui/dialog"
 import { DialogTitle } from "@radix-ui/react-dialog"
 
-import logoGoogle from "../../public/google.svg";
+import logoGoogle from "../../public/google.svg"
+import { authClient } from "../_lib/auth-client"
+import { useRouter } from "next/navigation";
+
 
 const SidebarButton = () => {
+  const { data: session } = authClient.useSession()
+
+  const router = useRouter()
+
+  async function handlerSignInWithGoogle() {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/",
+    })
+  }
+
+
+
+  
+  async function handlerLogoutandGoogle() {
+    await authClient.signOut({
+      fetchOptions:{
+        onSuccess: ()=>{
+          router.push("/");
+        }
+      }
+    })
+  }
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -42,44 +70,61 @@ const SidebarButton = () => {
           <SheetTitle className="text-start text-lg font-bold">Menu</SheetTitle>
         </SheetHeader>
 
-        {/*<div className="flex items-center justify-start gap-3 border-b border-solid py-5">
-              <Avatar>
-                <AvatarImage
-                  alt=""
-                  src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D"
-                />
-              </Avatar>
+        {session ? (
+          <div className="flex items-center justify-start gap-3 border-b border-solid py-5">
+            <Avatar>
+              <AvatarImage
+                alt=""
+                src={`${session.user?.image}`}
+              />
+            </Avatar>
 
-              <div className="flex flex-col">
-                <p className="truncate text-base font-bold">Maycon Cezar</p>
-                <p className="truncate text-xs font-normal">
-                  maycon.cezar.souza@gmail.com
-                </p>
-              </div>
-            </div>*/}
+            <div className="flex flex-col">
+              <p className="truncate text-base font-bold">
+                {session.user?.name}
+              </p>
+              <p className="truncate text-xs font-normal">
+                {session.user?.email}
+              </p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+            <h2 className="text-base font-bold">Olá. Faça seu login!</h2>
 
-        <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
-          <h2 className="text-base font-bold">Olá. Faça seu login!</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="sm">
+                  <LogInIcon size={18} />
+                </Button>
+              </DialogTrigger>
 
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button size="sm" >
-                <LogInIcon size={18} />
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="flex w-[90%] flex-col items-center">
-              <DialogTitle className="text-base font-bold">
-                Faça login na plataforma
-              </DialogTitle>
-              <DialogDescription className="text-sm text-gray-600">
-                Conecte-se usando sua conta do Google
-              </DialogDescription>
-              <Button variant="outline" className="w-full">
-                <Image alt="login usando conta do google" src={logoGoogle} width={18} height={18}/>
-                Google</Button>
-            </DialogContent>
-          </Dialog>
-        </div>
+              <DialogContent className="flex w-[90%] flex-col items-center">
+                <DialogTitle className="text-base font-bold">
+                  Faça login na plataforma
+                </DialogTitle>
+
+                <DialogDescription className="text-sm text-gray-600">
+                  Conecte-se usando sua conta do Google
+                </DialogDescription>
+
+                <Button
+                  variant="outline"
+                  className="w-full"
+                  onClick={handlerSignInWithGoogle}
+                >
+                  <Image
+                    alt="login usando conta do google"
+                    src={logoGoogle}
+                    width={18}
+                    height={18}
+                  />
+                  Google
+                </Button>
+              </DialogContent>
+            </Dialog>
+          </div>
+        )}
 
         <div className="flex flex-col gap-2 border-b border-solid py-5">
           <SheetClose>
@@ -125,6 +170,7 @@ const SidebarButton = () => {
           <Button
             className="flex items-center justify-start text-sm font-normal"
             variant="ghost"
+            onClick={handlerLogoutandGoogle}
           >
             <LogOut />
             Sair da conta
---
+++ app/_lib/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import { PrismaClient } from "../../generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import pg from "pg"
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
+
+const prisma = new PrismaClient({ adapter })
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+})
